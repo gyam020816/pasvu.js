c271abc6d6cd586f9ebeb01fcc4a125ec8fd5e03
Ha3 <git@ha3.eu>
2018-11-23 00:37:29 +0100

    (other) use real indentation

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Workr></Workr>
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <Workr></Workr>
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-import Workr from '@/components/Workr.vue';
+    import Vue from "vue";
+    import HelloWorld from "./components/HelloWorld.vue";
+    import Workr from "@/components/Workr.vue";
 
-export default Vue.extend({
-  name: 'app',
-  components: {
-    HelloWorld, Workr
-  }
-});
+    export default Vue.extend({
+        name: "app",
+        components: {
+            HelloWorld, Workr
+        }
+    });
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/Workr.vue
+++ src/components/Workr.vue
@@ -1,23 +1,22 @@
 <template>
-  <div class="workr">
-    this is the workr template
-  </div>
+    <div class="workr">
+        this is the workr template
+    </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+    import Vue from "vue";
 
-export default Vue.extend({
-  name: 'Workr',
-  props: {
-  },
-});
+    export default Vue.extend({
+        name: "Workr",
+        props: {},
+    });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.workr {
-  color: red;
-}
+    .workr {
+        color: red;
+    }
 
 </style>
