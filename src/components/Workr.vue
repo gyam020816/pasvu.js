24bacb0528ae34d9658e0303dc528d35c443994e
Ha3 <git@ha3.eu>
2018-11-23 00:52:52 +0100

    (evolve) animation transform base

--- src/components/Workr.vue
+++ src/components/Workr.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="workr">
         this is the workr template
+        <div class="wbase"></div>
     </div>
 </template>
 
@@ -17,6 +18,23 @@
 <style scoped>
     .workr {
         color: red;
+    }
+    .wbase {
+        border-top-left-radius: 40px;
+        border-top-right-radius: 40px;
+        border-bottom-left-radius: 20px;
+        border-bottom-right-radius: 20px;
+        border: 4px solid #CC0000;
+        background: red;
+        position: relative;
+        width: 100px;
+        height: 100px;
+
+        transform: rotate3d(1, 0, 0, 55deg) rotate3d(0, 0, 1, 55deg);
+        transition: 1s ease-in-out;
+    }
+    .wbase:hover {
+        transform: rotate3d(1, 0, 0, 55deg) rotate3d(0, 0, 1, 235deg);
     }
 
 </style>
