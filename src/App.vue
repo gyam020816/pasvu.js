2388237a2fa761e34958ef63b8d2adad196aa18a
Ha3 <git@ha3.eu>
2018-11-23 00:54:49 +0100

    (other) remove vue fluff

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
         <Workr></Workr>
-        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
     </div>
 </template>
 
