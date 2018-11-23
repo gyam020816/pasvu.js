695e6e5a1dffd52d37cb681d14403568614800dc
Ha3 <git@ha3.eu>
2018-11-23 22:34:44 +0100

    (evolve) add more 3d shenanigans ( https://davidwalsh.name/3d-transforms )

--- src/components/Workr.vue
+++ src/components/Workr.vue
@@ -4,7 +4,11 @@
         <div class="workr-anchor">
             <div class="wradius">X</div>
             <div class="wbase">F elt A</div>
-            <div class="wback">F elt B</div>
+            <div class="wback">
+                <div class="wback-l">L</div>
+                <div class="wback-c">C</div>
+                <div class="wback-r">R</div>
+            </div>
         </div>
     </div>
 </template>
@@ -55,6 +59,7 @@
     .workr-anchor:hover > .wbase {
         transform: rotate3d(1, 0, 0, 55deg) rotate3d(0, 0, 1, 535deg) rotate3d(0, 0, 1, -90deg);
     }
+
     .wback {
         position: absolute;
         top: 45px;
@@ -63,21 +68,58 @@
         margin-left: auto;
         margin-right: auto;
 
-        border-top-left-radius: 40px;
-        border-top-right-radius: 40px;
-        border-bottom-left-radius: 20px;
-        border-bottom-right-radius: 20px;
-        background: red;
+        border: 1px solid red;
         width: 100px;
         height: 75px;
 
         transform: rotate3d(1, 0, 0, 55deg) rotate3d(0, 0, 1, 55deg) rotate3d(0, 1, 0, 80deg)  rotate3d(0, 0, 1, -90deg);
         transform-origin: center center 55px;
         transition: 3s ease-in-out;
+        transform-style: preserve-3d;
     }
-    .workr-anchor:hover > .wback {
+
+    .wback-l, .wback-c, .wback-r {
+        position: absolute;
+        margin-left: auto;
+        margin-right: auto;
+
+        height: 75px;
+    }
+    .wback-l {
+        background: blue;
+        width: 30px;
+        left: 0;
+        right: auto;
+
+        border-top-left-radius: 40px;
+        border-bottom-left-radius: 20px;
+
+        transform: rotate3d(0, 1, 0, 10deg);
+        transform-origin: right center 0;
+    }
+    .wback-c {
+        background: red;
+        width: 40px;
+        left: 0;
+        right: 0;
+    }
+    .wback-r {
+        border-top-right-radius: 40px;
+        border-bottom-right-radius: 20px;
+        background: blue;
+        width: 30px;
+        left: auto;
+        right: 0;
+
+        transform: rotate3d(0, 1, 0, -10deg);
+        transform-origin: left center 0;
+    }
+    .workr-anchor:hover > .wback
+    {
         transform: rotate3d(1, 0, 0, 55deg) rotate3d(0, 0, 1, 535deg) rotate3d(0, 1, 0, 80deg) rotate3d(0, 0, 1, -90deg);
     }
+
+
     .wradius {
         position: absolute;
         top: 145px;
