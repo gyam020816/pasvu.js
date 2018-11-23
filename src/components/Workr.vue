25140b76c031d1b7841c2526e586dcb909fe3e7a
Ha3 <git@ha3.eu>
2018-11-23 23:09:16 +0100

    (evolve) use nested 3d elements

--- src/components/Workr.vue
+++ src/components/Workr.vue
@@ -2,12 +2,24 @@
     <div class="workr">
         this is the workr template
         <div class="workr-anchor">
-            <div class="wradius">X</div>
-            <div class="wbase">F elt A</div>
-            <div class="wback">
-                <div class="wback-l">L</div>
-                <div class="wback-c">C</div>
-                <div class="wback-r">R</div>
+            <span class="label">anchor</span>
+            <div class="workr-port">
+                <span class="label">port</span>
+                <div class="workr-mobile">
+                    <span class="label">mobile</span>
+                    <div class="wbase">F elt A</div>
+                    <div class="wback">
+                        <span class="label">back</span>
+                        <div class="wback-l">L</div>
+                        <div class="wback-c">C</div>
+                        <div class="wback-r">R</div>
+                    </div>
+                </div>
+                <div class="workr-static">
+                    <span class="label">static</span>
+                    <div class="wradius"></div>
+                    <div class="wfeet"></div>
+                </div>
             </div>
         </div>
     </div>
@@ -24,6 +36,13 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    .label {
+        position: absolute;
+        top: 0;
+        right: 0;
+        color: red;
+        font-size: 20px;
+    }
     .workr {
         color: red;
         border: 1px solid purple;
@@ -36,14 +55,42 @@
         color: white;
         font-size: 2em;
     }
+    .workr-port {
+        position: relative;
+        width: 200px;
+        height: 200px;
+
+        transform-style: preserve-3d;
+        transform: rotate3d(1, 0, 0, 55deg);
+
+        outline: 1px solid red;
+    }
+    .workr-mobile {
+        position: absolute;
+        width: 200px;
+        height: 200px;
+
+        transform-style: preserve-3d;
+        transition: 3s ease-in-out;
+        transform: rotate3d(0, 0, 1, 55deg);
+
+        outline: 1px solid blue;
+    }
+    .workr-static {
+        position: absolute;
+        width: 200px;
+        height: 200px;
+
+        transform-style: preserve-3d;
+        outline: 1px solid blue;
+    }
+    .workr-anchor:hover * .workr-mobile {
+        transform: rotate3d(0, 0, 1, 535deg);
+    }
     .wbase {
         position: absolute;
-        bottom: 5px;
-        left: 0;
-        right: 0;
-        margin-left: auto;
-        margin-right: auto;
-        overflow: hidden;
+        left: 50px;
+        top: 50px;
 
         border-top-left-radius: 20px;
         border-top-right-radius: 20px;
@@ -52,12 +99,6 @@
         background: black;
         width: 100px;
         height: 100px;
-
-        transform: rotate3d(1, 0, 0, 55deg) rotate3d(0, 0, 1, 55deg) rotate3d(0, 0, 1, -90deg);
-        transition: 3s ease-in-out;
-    }
-    .workr-anchor:hover > .wbase {
-        transform: rotate3d(1, 0, 0, 55deg) rotate3d(0, 0, 1, 535deg) rotate3d(0, 0, 1, -90deg);
     }
 
     .wback {
@@ -68,14 +109,15 @@
         margin-left: auto;
         margin-right: auto;
 
-        border: 1px solid red;
         width: 100px;
         height: 75px;
 
-        transform: rotate3d(1, 0, 0, 55deg) rotate3d(0, 0, 1, 55deg) rotate3d(0, 1, 0, 80deg)  rotate3d(0, 0, 1, -90deg);
+        transform: rotate3d(0, 0, 1, 90deg) rotate3d(0, 1, 0, 80deg)  rotate3d(0, 0, 1, -90deg);
         transform-origin: center center 55px;
-        transition: 3s ease-in-out;
         transform-style: preserve-3d;
+        transition: 3s ease-in-out;
+
+        outline: 1px solid green;
     }
 
     .wback-l, .wback-c, .wback-r {
@@ -84,9 +126,9 @@
         margin-right: auto;
 
         height: 75px;
+        background: black;
     }
     .wback-l {
-        background: blue;
         width: 30px;
         left: 0;
         right: auto;
@@ -98,7 +140,6 @@
         transform-origin: right center 0;
     }
     .wback-c {
-        background: red;
         width: 40px;
         left: 0;
         right: 0;
@@ -106,7 +147,6 @@
     .wback-r {
         border-top-right-radius: 40px;
         border-bottom-right-radius: 20px;
-        background: blue;
         width: 30px;
         left: auto;
         right: 0;
@@ -114,19 +154,12 @@
         transform: rotate3d(0, 1, 0, -10deg);
         transform-origin: left center 0;
     }
-    .workr-anchor:hover > .wback
-    {
-        transform: rotate3d(1, 0, 0, 55deg) rotate3d(0, 0, 1, 535deg) rotate3d(0, 1, 0, 80deg) rotate3d(0, 0, 1, -90deg);
-    }
 
 
     .wradius {
         position: absolute;
-        top: 145px;
-        left: 0;
-        right: 0;
-        margin-left: auto;
-        margin-right: auto;
+        right: 90px;
+        bottom: -10px;
 
         border-top-left-radius: 40px;
         border-top-right-radius: 40px;
@@ -136,7 +169,6 @@
         width: 20px;
         height: 75px;
 
-        transform: rotate3d(1, 0, 0, 55deg) rotate3d(0, 0, 1, 90deg) rotate3d(0, 1, 0, 90deg)  rotate3d(0, 0, 1, -90deg);
-        transition: 3s ease-in-out;
+        transform: rotate3d(0, 0, 1, 90deg) rotate3d(0, 1, 0, 90deg)  rotate3d(0, 0, 1, -90deg);
     }
 </style>
